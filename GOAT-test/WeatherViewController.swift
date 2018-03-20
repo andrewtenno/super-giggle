bc9b0fd6d22e8256c1fa136b18481b66e74bb84b
Andrew Tenno <andrew.tenno@gmail.com>
2018-03-20 11:19:35 -0700

    Initial Commit

---
+++ GOAT-test/WeatherViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GOAT-test
+//
+//  Created by Andrew Tenno on 3/20/18.
+//  Copyright © 2018 Andrew Tenno. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GOAT-testTests/GOAT_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  GOAT_testTests.swift
+//  GOAT-testTests
+//
+//  Created by Andrew Tenno on 3/20/18.
+//  Copyright © 2018 Andrew Tenno. All rights reserved.
+//
+
+import XCTest
+@testable import GOAT_test
+
+class GOAT_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
